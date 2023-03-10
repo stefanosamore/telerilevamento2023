f83c195e14e6619882cbd85784180c5898d5e3bf
stefanosamore <127400832+stefanosamore@users.noreply.github.com>
2023-03-10 20:17:40 +0100

    Update R_firstcode.R

--- R_firstcode.R
+++ R_firstcode.R
@@ -2,8 +2,9 @@
 # Let's install the raster package
 
 intall.packages("raster")
-
 library("raster")
 
-
-
+# import data, setting the working directory
+setwd("/User/stefano/desktop/lab")
+l2011 <- brick("p224r63_2011_masked.grd")
+plot(l2011)
