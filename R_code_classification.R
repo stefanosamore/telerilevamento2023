e941674ef5b10c3169268fe7fbd6f7e47d1aea2a
stefanosamore <127400832+stefanosamore@users.noreply.github.com>
2023-04-21 16:24:44 +0200

    Update R_code_classification.R

--- R_code_classification.R
+++ R_code_classification.R
@@ -4,3 +4,47 @@
 #install.packages("devtools")
 
 install_github("bleutner/RStoolbox")
+
+Grand Canyon exercise
+
+
+library(raster)
+
+setwd("/Users/stefanosamore/desktop/lab")
+brick("dolansprings_oli_2013088_canyon_lrg.jpg")
+ 
+brick("dolansprings_oli_2013088_canyon_lrg.jpeg")
+
+gc <- brick("dolansprings_oli_2013088_canyon_lrg.jpg")
+gc <- brick("dolansprings_oli_2013088_canyon_lrg.jpeg")
+
+gc
+
+plotRGB(gc, r=1, g=2, b=3, stretch="lin") 
+ plotRGB(gc, r=1, g=2, b=3, stretch="hist")
+
+plotRGB(gc, r=1, g=2, b=3, stretch="lin")
+
+crop(gc, drawExtent())
+
+gcc<-crop(gc, drawExtent())
+
+gcc<-crop(gc, drawExtent())
+
+plotRGB(gcc, r=1, g=2, b=3, stretch="lin")
+ncll(gcc)
+
+ncell(gcc)
+ncell(gc)
+
+
+getValues(gc)
+singlenr <- getValues(gc)
+singlenr
+
+
+
+kmeans(singlenr, centers = 3)
+kcluster <- kmeans(singlenr, centers = 3)
+kcluster
+
